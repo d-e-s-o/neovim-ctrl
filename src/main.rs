991a36199e87ebd3ed01fb629fb094f5a559ddc8
Daniel Mueller <deso@posteo.net>
2019-02-01 22:57:16 -0800

    Introduce basic error handling infrastructure
    This change introduces the basic error handling infrastructure to the
    program. For lack of stabilization of the process::Termination we work
    fairly hard to make do with just returning a Result from main. To
    accommodate that we introduce an ExitError struct whose main purpose is
    to provide a proper error representation to the user.

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,59 @@
 // * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
 // *************************************************************************
 
+//! `neovim-ctrl` is a program for finding and interacting with a Neovim
+//! instance running in the current terminal.
 
-fn main() {
-  println!("Hello, world!");
+use std::env::args_os;
+use std::result::Result as StdResult;
+
+
+mod int {
+  use std::fmt::Debug;
+  use std::fmt::Display;
+  use std::fmt::Formatter;
+  use std::fmt::Result;
+
+  pub enum Error {
+    UsageError,
+  }
+
+  impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+      match self {
+        Error::UsageError => write!(
+          f,
+          "Usage: nvim-ctrl tty"
+        ),
+      }
+    }
+  }
+
+
+  // An error class for the purpose of being able to return a Result with
+  // a sane error representation from `main`.
+  pub struct ExitError(pub Error);
+
+  impl From<Error> for ExitError {
+    fn from(e: Error) -> Self {
+      Self(e)
+    }
+  }
+
+  impl Debug for ExitError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+      // For our intents and purposes the debug representation behaves
+      // exactly like Display would, by printing a correctly formatted
+      // error. This implementation is what is actually invoked when
+      // displaying an error returned from `main`.
+      write!(f, "{}", self.0)
+    }
+  }
+}
+
+
+fn main() -> StdResult<(), int::ExitError> {
+  let mut argv = args_os().skip(1);
+  let _tty = argv.next().ok_or_else(|| int::Error::UsageError)?;
+  Ok(())
 }
